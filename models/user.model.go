8e90f99921e2fbd481ecd1c9008b64e5b5decbfd
mpavithran <mpavithran1998@gmail.com>
2025-02-26 10:43:27 +0530

    fix(user model): deleted_at column fix

--- models/user.model.go
+++ models/user.model.go
@@ -1,9 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	ID        uint   `gorm:"primarykey"`
+	Name      string `json:"name"`
+	Email     string `json:"email" gorm:"unique"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
